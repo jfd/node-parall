1f3c626017e6e5fec15dbc91b31c59ae15537809
Johan Dahlberg <dahlberg.johan@gmail.com>
2010-09-17 18:08:05 +0200

    Added matching module and simple test

---
+++ test/matching/test-compile.js
@@ -0,0 +1,6 @@
+const ok                = require("assert").ok
+    , equal             = require("assert").equal
+    , build           = require("../../lib/matching").build
+    
+    
+console.log(build(["testa", "testb", 1]));
