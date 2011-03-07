764d4fa35076c499e9b17263b5cfc002a4ef0c09
Johan Dahlberg <dahlberg.johan@gmail.com>
2011-03-07 13:37:27 +0100

    fix reject-receive test

--- test/messaging/test-reject-receive.js
+++ test/messaging/test-reject-receive.js
@@ -1,25 +1,24 @@
 const equal             = require("assert").equal
     , createChannel     = require("../../lib").createChannel
     , spawn             = require("../../lib").spawn
-    , receive           = require("../../lib").receive
+    , openStdMsg        = require("../../lib/util").openStdMsg
     , timeout           = require("../common").timeout
     , shutdown          = require("../common").shutdown
 
 var master  = null
+var req;
 
 timeout(5000);
 
 if (process.argv[2] == "worker") {
-  receive (
-    "ping", function() {
-      this.reject();
-    }
-  )
+  openStdMsg().receive = function ping(msg) {
+    msg.reject();
+  };
 } else {
   master = createChannel("req");
   master.attach(spawn(__filename, ["worker"]));
-  master.send("ping", function(msg, state) {
-    equal(state, 'REJECT');
+  req = master.send("ping");
+  req.receive = function REJECT() {
     shutdown();
-  });
+  };
 }
